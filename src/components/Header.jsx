0daa3e01201c156b24643c196c746d3b39cbb4e7
unknown <lorena0717@hotmail.com>
2021-09-23 11:41:13 -0500

    detallando lupa, según diseño

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,26 @@
 import React from "react";
 import logo from "../assest/logo.png";
-import lupa from "../assest/lupa.png";
-import perfil from '../assest/perfil.png'
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap-icons';
-// import 'bootstrap/dist/css/bootstrap.css';'
-
-// import carrito from "../assest/carrito.png";
+import perfil from "../assest/perfil.png";
+import { BsSearch } from "react-icons/bs";
 
 function Header() {
   return (
     <div className="contenedorNav">
       <img src={logo} alt="logo" width="200"></img>
-      {/* <nav className='nav'> */}
-          {/* <form 
-          // className="d-flex"
-          > */}
-            <div className="contentbuscador">
-              <input
-              className='buscador'
-               
-              //  className="form-control me-2"
-                type="search"
-                placeholder='Busca marcas y productos...  '
-                aria-label="Search"
-              />
-              <button
-              className='btnBuscador'
-              //  className="btn btn-outline-success"
-                type="submit">
-                  {/* <img className='lupa' src={lupa} alt='imgperfil'></img> */}
-              <i class="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </div>
-          {/* </form> */}
-           <div className="datos">
-           <h3>Lorena</h3>
+      <div className="contentbuscador">
+        <input
+          className="buscador"
+          type="search"
+          placeholder="Busca marcas y productos...  "
+          aria-label="Search"
+        />
+        <BsSearch className="lupa" />
+      </div>
+      <div className="datos">
+        <h3>Lorena</h3>
 
-            <img className='imgPerfil' src={perfil} alt='imgperfil'></img>
-          
-          </div>
-     
+        <img className="imgPerfil" src={perfil} alt="imgperfil"></img>
+      </div>
     </div>
   );
 }
